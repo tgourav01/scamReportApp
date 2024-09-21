4eaaf4bd0b71ad9e99fda2506c136d3fdfa1195a
Gourav Thakur <gouravthakur@Gouravs-MacBook-Air.local>
2024-09-22 01:11:39 +0530

    Removed all files from git tracking

--- IdeaProjects/scamReportApp/src/main/java/com/gourav/scamReportApp/repository/EmailRepo.java
+++
@@ -1,8 +0,0 @@
-package com.gourav.scamReportApp.repository;
-
-
-import com.gourav.scamReportApp.model.Email;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmailRepo extends JpaRepository<Email,String> {
-}
--- IdeaProjects/scamReportApp/src/main/java/com/gourav/scamReportApp/repository/NumberRepo.java
+++
@@ -1,7 +0,0 @@
-package com.gourav.scamReportApp.repository;
-
-import com.gourav.scamReportApp.model.Number;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NumberRepo extends JpaRepository<Number,Long> {
-}
